e6be7f1b91302890134930657174e75f9c373f54
ale.acevedo <ale.acevedo@live.com.ar>
2016-10-04 21:43:25 -0300

    YA ANDA

--- hash/hash.c
+++ hash/hash.c
@@ -1,4 +1,10 @@
 #include "hash.h"
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "fnv.h"
 
 #define TAM_INICIAL (100)
 
@@ -19,7 +25,6 @@ struct hash{
   nodo_hash_t *indice;
   size_t largo;
   size_t cant_elementos;
-  size_t borrados;
   size_t n_mersenne;
 };
 
@@ -41,27 +46,20 @@ nodo_hash_t *avanzar_nodo(const hash_t *hash, nodo_hash_t *nodo_hash, const char
   return nodo_hash;
 }
 
-nodo_hash_t *encontrar_nodo(const hash_t *hash, nodo_hash_t *nodo_hash, const char* clave, Fnv32_t hash_clave){
-  Fnv32_t primer_hash = hash_clave;
-  int i =0;
-  while(nodo_hash->clave==NULL){
-    i++;
-    nodo_hash = avanzar_nodo(hash, nodo_hash, clave, &hash_clave);
-    if(hash_clave==primer_hash){
-      return NULL;
-    }
-  }
-  while(strcmp(nodo_hash->clave,clave)!=0){
-    nodo_hash = avanzar_nodo(hash, nodo_hash, clave, &hash_clave);
-    if(hash_clave==primer_hash){
-      return NULL;
-    }
-    while(nodo_hash->clave==NULL){
-      nodo_hash = avanzar_nodo(hash, nodo_hash, clave, &hash_clave);
-      if(hash_clave==primer_hash){
-        return NULL;
+nodo_hash_t *encontrar_nodo(const hash_t *hash, const char* clave){
+  Fnv32_t hash_clave = fnv_32_str(clave, FNV1_32_INIT);
+  hash_clave = hash_clave % (int)hash->largo;
+  nodo_hash_t *nodo_hash = &(hash->indice[hash_clave]);
+  while(nodo_hash->estado!=vacio){
+    if(nodo_hash->estado==ocupado){
+      if(strcmp(nodo_hash->clave,clave)==0){
+        return nodo_hash;
       }
     }
+    nodo_hash = avanzar_nodo(hash, nodo_hash, clave, &hash_clave);
+  }
+  if(nodo_hash->estado==vacio){
+    return nodo_hash;
   }
   return nodo_hash;
 }
@@ -80,7 +78,7 @@ hash_t *hash_crear_tam_mod(hash_destruir_dato_t destruir_dato, size_t tam){
   return hash;
 }
 
-bool hash_redimencionar(hash_t *hash, size_t tam_nuevo){
+bool hash_redimensionar(hash_t *hash, size_t tam_nuevo){
   nodo_hash_t *aux = hash->indice;
   size_t tam_aux = hash->largo;
   hash->indice = calloc(tam_nuevo, sizeof(nodo_hash_t));
@@ -90,13 +88,10 @@ bool hash_redimencionar(hash_t *hash, size_t tam_nuevo){
     return false;
   }
   hash->cant_elementos = 0;
-  hash->borrados = 0;
   hash->largo=tam_nuevo;
   for(int i = 0; i<tam_aux; i++){
     if(aux[i].estado==ocupado){
       hash_guardar(hash,aux[i].clave,aux[i].dato);
-    }
-    if(aux->estado!=vacio){
       free(aux[i].clave);
     }
   }
@@ -105,7 +100,7 @@ bool hash_redimencionar(hash_t *hash, size_t tam_nuevo){
 }
 
 size_t calcular_carga(hash_t *hash){
-  size_t resultado = ((hash->borrados + hash->cant_elementos)/(hash->largo))*100;
+  size_t resultado = ((hash->cant_elementos)/(hash->largo))*100;
   return resultado;
 }
 
@@ -114,83 +109,64 @@ hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
 }
 
 bool hash_guardar(hash_t *hash, const char* clave, void* dato){
-  Fnv32_t hash_clave = fnv_32_str(clave, FNV1_32_INIT);
-  hash_clave = hash_clave % (int)hash->largo;
-  nodo_hash_t *nodo_hash = &(hash->indice[hash_clave]);
-  int comp = 0;
-  while(nodo_hash->estado == ocupado){
-    comp=strcmp(nodo_hash->clave,clave);
-    if(comp == 0){
-      nodo_hash->dato = dato;
-      return true;
-    }
-    if(hash_clave<hash->largo-1){
-      nodo_hash++;
-      hash_clave++;
-    }
-    else{
-      nodo_hash = hash->indice;
-      hash_clave = 0;
-    }
+  nodo_hash_t *nodo_hash = encontrar_nodo(hash, clave);
+  if(nodo_hash->estado==ocupado){
+    if(hash->destruir_dato!=NULL){
+        hash->destruir_dato(nodo_hash->dato);
+      }
+    nodo_hash->dato=dato;
+    return true;
   }
-  if(nodo_hash->estado==vacio){
-    nodo_hash->clave = calloc(strlen(clave)+1,sizeof(char));
-    if(nodo_hash->clave==NULL){
-      return false;
-    }
+  nodo_hash->clave = calloc(strlen(clave)+1,sizeof(char));
+  if(nodo_hash->clave==NULL){
+    return false;
   }
   for(int i = 0; i<strlen(clave); i++){
     nodo_hash->clave[i] = clave[i];
-
   }
   nodo_hash->dato = dato;
   hash->cant_elementos+=1;
   nodo_hash->estado = ocupado;
-  if(calcular_carga(hash)>70){
-    hash_redimencionar(hash, hash->largo*10);
+  if(calcular_carga(hash)>80){
+    printf("DEBUG Redim UP\n");
+    hash_redimensionar(hash, hash->largo*5);
   }
   return true;
 }
 
 void *hash_borrar(hash_t *hash, const char *clave){
-  Fnv32_t hash_clave = fnv_32_str(clave, FNV1_32_INIT);
-  hash_clave = hash_clave % (int)hash->largo;
-  nodo_hash_t *nodo_hash = &(hash->indice[hash_clave]);
   void *dato;
-  nodo_hash = encontrar_nodo(hash, nodo_hash, clave, hash_clave);
-  if(nodo_hash==NULL){
-    return NULL;
-  }
-  if(nodo_hash->estado==borrado){
+  nodo_hash_t *nodo_hash = encontrar_nodo(hash, clave);
+  if(nodo_hash->estado!=ocupado){
     return NULL;
   }
   nodo_hash->estado = borrado;
+  free(nodo_hash->clave);
   hash->cant_elementos-=1;
-  hash->borrados++;
   dato = nodo_hash->dato;
   if(calcular_carga(hash)<5){
-    hash_redimencionar(hash, (hash->largo)/2);
+    printf("DEBUG Redim DW\n");
+    if(hash->cant_elementos==0){
+      hash_redimensionar(hash, TAM_INICIAL);
+    }
+    else{
+      hash_redimensionar(hash, (size_t)((double)hash->cant_elementos/0.3));
+    }
   }
   return dato;
 }
 
 void *hash_obtener(const hash_t *hash, const char *clave){
-  Fnv32_t hash_clave = fnv_32_str(clave, FNV1_32_INIT);
-  hash_clave = hash_clave % (int)hash->largo;
-  nodo_hash_t *nodo_hash = &(hash->indice[hash_clave]);
-  nodo_hash = encontrar_nodo(hash, nodo_hash, clave, hash_clave);
-  if(nodo_hash==NULL || nodo_hash->estado==borrado){
+  nodo_hash_t *nodo_hash = encontrar_nodo(hash, clave);
+  if(nodo_hash->estado!=ocupado){
     return NULL;
   }
   return nodo_hash->dato;
 }
 
 bool hash_pertenece(const hash_t *hash, const char *clave){
-  Fnv32_t hash_clave = fnv_32_str(clave, FNV1_32_INIT);
-  hash_clave = hash_clave % (int)hash->largo;
-  nodo_hash_t *nodo_hash = &(hash->indice[hash_clave]);
-  nodo_hash = encontrar_nodo(hash, nodo_hash, clave, hash_clave);
-  if(nodo_hash==NULL || nodo_hash->estado==borrado){
+  nodo_hash_t *nodo_hash = encontrar_nodo(hash, clave);
+  if(nodo_hash->estado!=ocupado){
     return false;
   }
   return true;
@@ -204,9 +180,9 @@ size_t hash_cantidad(const hash_t *hash){
 void hash_destruir(hash_t *hash){
   nodo_hash_t *nodo_hash = hash->indice;
   for(int i = 0; i<hash->largo; i++){
-    if(nodo_hash->estado != vacio){
+    if(nodo_hash->estado == ocupado){
       free(nodo_hash->clave);
-      if(nodo_hash->estado == ocupado && hash->destruir_dato!=NULL){
+      if(hash->destruir_dato!=NULL){
         hash->destruir_dato(nodo_hash->dato);
       }
     }
@@ -224,10 +200,11 @@ hash_iter_t *hash_iter_crear(const hash_t *hash){
   hash_iter->hash = hash;
   hash_iter->actual = hash->indice;
   hash_iter->pos = 1;
-  if(hash_iter->actual == NULL){
-    while(hash_iter->actual->estado!=ocupado){
+  if(hash_iter->hash->cant_elementos==0){
+    return hash_iter;
+  }
+  while(hash_iter->actual->estado!=ocupado){
     hash_iter->actual++;
-    }
   }
   return hash_iter;
 }
@@ -241,6 +218,7 @@ bool hash_iter_avanzar(hash_iter_t *iter){
     iter->actual = NULL;
     return true;
   }
+  iter->actual++;
   while(iter->actual->estado!=ocupado){
     iter->actual++;
   }
