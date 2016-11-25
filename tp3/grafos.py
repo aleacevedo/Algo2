9f33b5ea5096d1f129b39ec5b6605e4a86b038e9
ale.acevedo <ale.acevedo@live.com.ar>
2016-11-25 13:30:45 -0300

    modificado: tp3/grafos.py

--- tp3/grafos.py
+++ tp3/grafos.py
@@ -2,7 +2,6 @@ class _Vertice:
     def __init__(self, id = None, valor = None, adyacencias = {}):
         self.id = id
         self.valor = valor
-        self.adyacencias = adyacencias
 
 class _IteradorGrafo:
     def __init__(self, vertices, ids):
@@ -21,6 +20,7 @@ class _IteradorGrafo:
 class Grafos:
     def __init__(self, es_dirigido = False):
         self.vertices = {}
+        self.adyacencias = {}
         self.largo = 0
 
     def __len__(self):
@@ -38,4 +38,5 @@ class Grafos:
         else:
             self.vertices[id].valor = valor
 
-    
+    def __delitem__(self, id):
+        if(self.vertices.get(id)==None):
